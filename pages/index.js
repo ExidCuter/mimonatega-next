3b2188405adfebbf5ceef52cceb11ba63cb93433
ddodovic <d@td-fl.org>
2024-01-02 16:55:12 +0100

    fixed empty prices error

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,6 @@
 import Head from 'next/head'
 import {Badge, Box, Button, Container, Grid, InputAdornment, TextField, Typography} from "@mui/material";
-import ItemsList from "../components/ItemsList";
-import {useEffect, useState} from "react";
-import axios from "axios";
-import {config} from '../config/Constants'
-import FakeItemList from "../components/FakeItemList";
+import {useState} from "react";
 import {Extension, Search} from "@mui/icons-material";
 import { useRouter } from 'next/router';
 import MimonategaBar from "../components/MimonategaBar";
@@ -15,18 +11,7 @@ function Home() {
     const urlRegex = new RegExp(/^(ftp|http|https):\/\/[^ "]+$/);
 
     const router = useRouter();
-
-    const [items, setItems] = useState([]);
-    const [loading, setLoading] = useState(true);
     const [searchError, setSearchError] = useState(false);
-
-    useEffect(() => {
-        axios.get(`${config.url}/api/v1/items/promoted`)
-            .then(res => {
-                setItems(res.data.data);
-                setLoading(false);
-            });
-    }, []);
 
     const getSearchResults = (query) => {
         if (isUrl(query)) {
@@ -94,7 +79,6 @@ function Home() {
                                 }}/>
                         </Grid>
                         <Grid item md={2}/>
-
                         <Grid item md={2}></Grid>
                         <Grid item md={8} xs={12}>
                             <Typography sx={{mt: '1em'}} variant={"subtitle1"}>Na voljo tudi vtičnik za brskalnike!</Typography>
@@ -102,12 +86,6 @@ function Home() {
                         </Grid>
                         <Grid item md={2}></Grid>
                     </Grid>
-                </div>
-                <div style={{marginTop: "5em"}}>
-                    <Typography variant={"h5"} style={{marginBottom: 20}}>
-                        Promocijski izdelki
-                    </Typography>
-                    {loading ? <FakeItemList/> : <ItemsList items={items}/>}
                 </div>
             </Container>
         </>
